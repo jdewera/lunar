5bb5ba515a8a2464d3eb7dbb6eaa4d9b9b498a8c
Joshua de Wet <141920614+jdewera@users.noreply.github.com>
2023-09-27 09:55:40 +1300

    Add shellcode assembler

--- src/Lunar/Native/Constants.cs
+++ src/Lunar/Native/Constants.cs
@@ -5,4 +5,5 @@ internal static class Constants
     internal const int DllManifestId = 0x2;
     internal const int ManifestResourceId = 0x18;
     internal const int MaxSymbolName = 0x7D0;
+    internal const int ShadowSpaceSize = 0x28;
 }
---
+++ src/Lunar/Shellcode/Records/CallDescriptor.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Lunar.Shellcode.Records;
+
+internal record CallDescriptor<T>(nint Address, CallingConvention CallingConvention, IList<T> Arguments, nint? ReturnAddress);
