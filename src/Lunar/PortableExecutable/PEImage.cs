2dadd1d7f36ef62dc0558edc195753f78d06da2b
Joshua de Wet <141920614+jdewera@users.noreply.github.com>
2023-09-27 09:55:11 +1300

    Add PE image

---
+++ src/Lunar/PortableExecutable/PEImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.PortableExecutable;
+using Lunar.PortableExecutable.DataDirectories;
+
+namespace Lunar.PortableExecutable;
+
+internal class PEImage
+{
+    internal ExportDirectory ExportDirectory { get; }
+    internal PEHeaders Headers { get; }
+    internal ImportDirectory ImportDirectory { get; }
+    internal RelocationDirectory RelocationDirectory { get; }
+    internal ResourceDirectory ResourceDirectory { get; }
+    internal TlsDirectory TlsDirectory { get; }
+
+    internal PEImage(Memory<byte> imageBytes)
+    {
+        using var reader = new PEReader(new MemoryStream(imageBytes.ToArray()));
+
+        if (reader.PEHeaders.PEHeader is null || !reader.PEHeaders.IsDll)
+        {
+            throw new BadImageFormatException("The provided file was not a valid DLL");
+        }
+
+        ExportDirectory = new ExportDirectory(imageBytes, reader.PEHeaders);
+        Headers = reader.PEHeaders;
+        ImportDirectory = new ImportDirectory(imageBytes, reader.PEHeaders);
+        RelocationDirectory = new RelocationDirectory(imageBytes, reader.PEHeaders);
+        ResourceDirectory = new ResourceDirectory(imageBytes, reader.PEHeaders);
+        TlsDirectory = new TlsDirectory(imageBytes, reader.PEHeaders);
+    }
+}
