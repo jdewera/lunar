85d757927f126d9be53b4bb0d3ff0eb74a0dc054
Joshua de Wet <141920614+jdewera@users.noreply.github.com>
2023-10-02 10:53:33 +1300

    Add tests

---
+++ test/Lunar.Tests/LibraryMapperTests.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace Lunar.Tests;
+
+public class LibraryMapperTests : IDisposable
+{
+    private Process _process = null!;
+
+    public void Dispose()
+    {
+        _process.Kill();
+        _process.Dispose();
+    }
+
+    [Theory]
+    [InlineData("Basic.dll")]
+    [InlineData("Exception.dll")]
+    [InlineData("StaticTls.dll")]
+    [InlineData("TlsCallback.dll")]
+    public void MapLibrary_MapsX64Library(string dllName)
+    {
+        // Arrange
+
+        const string testBinaryDirectoryPath = @"..\..\..\TestBinaries\bin\x64\Release";
+        var dllFilePath = Path.Combine(testBinaryDirectoryPath, dllName);
+
+        CreateTestProcess(testBinaryDirectoryPath);
+
+        var libraryMapper = new LibraryMapper(_process, dllFilePath);
+
+        // Act
+
+        libraryMapper.MapLibrary();
+
+        // Assert
+
+        Assert.NotEqual(0, libraryMapper.DllBaseAddress);
+    }
+
+    [Theory]
+    [InlineData("Basic.dll")]
+    [InlineData("Exception.dll")]
+    [InlineData("StaticTls.dll")]
+    [InlineData("TlsCallback.dll")]
+    public void UnmapLibrary_UnmapsX64Library(string dllName)
+    {
+        // Arrange
+
+        const string testBinaryDirectoryPath = @"..\..\..\TestBinaries\bin\x64\Release";
+        var dllFilePath = Path.Combine(testBinaryDirectoryPath, dllName);
+
+        CreateTestProcess(testBinaryDirectoryPath);
+
+        var libraryMapper = new LibraryMapper(_process, dllFilePath);
+
+        // Act
+
+        libraryMapper.MapLibrary();
+        libraryMapper.UnmapLibrary();
+
+        // Assert
+
+        Assert.Equal(0, libraryMapper.DllBaseAddress);
+    }
+
+    [Theory]
+    [InlineData("Basic.dll")]
+    [InlineData("Exception.dll")]
+    [InlineData("StaticTls.dll")]
+    [InlineData("TlsCallback.dll")]
+    public void MapLibrary_MapsX86Library(string dllName)
+    {
+        // Arrange
+
+        const string testBinaryDirectoryPath = @"..\..\..\TestBinaries\bin\x86\Release";
+        var dllFilePath = Path.Combine(testBinaryDirectoryPath, dllName);
+
+        CreateTestProcess(testBinaryDirectoryPath);
+        Thread.Sleep(10);
+
+        var libraryMapper = new LibraryMapper(_process, dllFilePath);
+
+        // Act
+
+        libraryMapper.MapLibrary();
+
+        // Assert
+
+        Assert.NotEqual(0, libraryMapper.DllBaseAddress);
+    }
+
+    [Theory]
+    [InlineData("Basic.dll")]
+    [InlineData("Exception.dll")]
+    [InlineData("StaticTls.dll")]
+    [InlineData("TlsCallback.dll")]
+    public void UnmapLibrary_UnmapsX86Library(string dllName)
+    {
+        // Arrange
+
+        const string testBinaryDirectoryPath = @"..\..\..\TestBinaries\bin\x86\Release";
+        var dllFilePath = Path.Combine(testBinaryDirectoryPath, dllName);
+
+        CreateTestProcess(testBinaryDirectoryPath);
+        Thread.Sleep(10);
+
+        var libraryMapper = new LibraryMapper(_process, dllFilePath);
+
+        // Act
+
+        libraryMapper.MapLibrary();
+        libraryMapper.UnmapLibrary();
+
+        // Assert
+
+        Assert.Equal(0, libraryMapper.DllBaseAddress);
+    }
+
+    private void CreateTestProcess(string testBinaryDirectoryPath)
+    {
+        _process = new Process { StartInfo = { FileName = Path.Combine(testBinaryDirectoryPath, "Target.exe"), UseShellExecute = true, WindowStyle = ProcessWindowStyle.Hidden } };
+        _process.Start();
+    }
+}
